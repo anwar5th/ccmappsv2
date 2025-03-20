6e9355fccc4c597a51ec142726243054fa197332
anwar5th <anwar5th@gmail.com>
2025-03-20 16:34:32 +0700

    membat menu

--- app/Http/Controllers/PoController.php
+++ app/Http/Controllers/PoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Tb_po;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class PoController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PoController;
+use App\Http\Controllers\SpkController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/dashboard', function () { return view('dashboard'); 
+Route::get('/po', function () { return view('<p>test</p>'); });
+
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
